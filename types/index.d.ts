48f91920ecf9be5259edc10841624a6dc3cb2074
Arvid Ternblom <arvidternblom@Arvids-MacBook-Pro.local>
2018-11-23 11:00:25 +0800

    added typescript types

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+/**
+ * This plugin defines a global device object, which describes the device's hardware and software.
+ * Although the object is in the global scope, it is not available until after the deviceready event.
+ */
+interface TFDetector {
+    /** Get the version of Cordova running on the device. */
+    cordova: string;
+    /** Indicates that Cordova initialize successfully. */
+    available: boolean;
+    /** Get the device's operating system name. */
+    platform: string;
+	/** Get the device's manufacturer. */
+	manufacturer: string;
+	/** Whether the device is running on a simulator. */
+	isTestFlight: boolean;}
+
+declare var device: TFDetector;
